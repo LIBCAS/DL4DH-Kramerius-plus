6afd1e200e0e463a22c8a6c8baf40542e55327b9
Norbert Bodnár <bodnar@inqool.cz>
2021-04-14 11:27:50 +0200

    Project created

---
+++ api/src/main/java/cz/inqool/dl4dh/kramerius_plus/KrameriusPlusApplication.java
@@ -0,0 +1,13 @@
+package cz.inqool.dl4dh.kramerius_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrameriusPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrameriusPlusApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/cz/inqool/dl4dh/kramerius_plus/KrameriusPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.inqool.dl4dh.kramerius_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrameriusPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
