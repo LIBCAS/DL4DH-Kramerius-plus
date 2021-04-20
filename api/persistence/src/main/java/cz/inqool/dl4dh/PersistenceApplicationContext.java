a51f0f82c2b2e6828cd737a43119ff68c80f9a2e
Norbert Bodnár <bodnar@inqool.cz>
2021-04-20 12:39:08 +0200

    Added PersistenceApplicationContext

---
+++ api/persistence/src/main/java/cz/inqool/dl4dh/PersistenceApplicationContext.java
@@ -0,0 +1,15 @@
+package cz.inqool.dl4dh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Norbert Bodnar
+ */
+@SpringBootApplication
+public class PersistenceApplicationContext {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceApplicationContext.class);
+    }
+}
