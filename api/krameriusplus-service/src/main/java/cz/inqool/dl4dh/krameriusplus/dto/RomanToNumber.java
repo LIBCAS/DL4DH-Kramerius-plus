3c14eb2be2fe078687a7e570978269c21fee6815
Norbert Bodnár <bodnar@inqool.cz>
2021-05-25 09:45:53 +0200

    Refactored RomanToNumber class

--- api/krameriusplus-service/src/main/java/cz/inqool/dl4dh/krameriusplus/dto/RomanToNumber.java
+++ api/krameriusplus-service/src/main/java/cz/inqool/dl4dh/krameriusplus/dto/RomanToNumber.java
@@ -4,57 +4,55 @@ package cz.inqool.dl4dh.krameriusplus.dto;
  * @author Norbert Bodnar
  */
 public class RomanToNumber {
-    // This function returns
-    // value of a Roman symbol
-    public static int value(char r) {
-        if (r == 'I')
-            return 1;
-        if (r == 'V')
-            return 5;
-        if (r == 'X')
-            return 10;
-        if (r == 'L')
-            return 50;
-        if (r == 'C')
-            return 100;
-        if (r == 'D')
-            return 500;
-        if (r == 'M')
-            return 1000;
-        throw new IllegalArgumentException("Not a roman character");
+    // This function returns a value of a Roman symbol
+    public static int value(char symbol) {
+        switch (symbol) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                throw new IllegalArgumentException("Not a roman character");
+        }
     }
 
-    // Finds decimal value of a
-    // given romal numeral
-    public static int romanToDecimal(String str) {
+    // Finds decimal value of a given roman numeral
+    public static int romanToDecimal(String romanNumber) {
         // Initialize result
-        int res = 0;
+        int result = 0;
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < romanNumber.length(); i++) {
             // Getting value of symbol s[i]
-            int s1 = value(str.charAt(i));
+            int currentSymbol = value(romanNumber.charAt(i));
 
             // Getting value of symbol s[i+1]
-            if (i + 1 < str.length()) {
-                int s2 = value(str.charAt(i + 1));
+            if (i + 1 < romanNumber.length()) {
+                int nextSymbol = value(romanNumber.charAt(i + 1));
 
                 // Comparing both values
-                if (s1 >= s2) {
-                    // Value of current symbol
-                    // is greater or equalto
-                    // the next symbol
-                    res = res + s1;
+                if (currentSymbol >= nextSymbol) {
+                    // Value of current symbol is greater or equal to the next symbol
+                    result = result + currentSymbol;
                 } else {
-                    // Value of current symbol is
-                    // less than the next symbol
-                    res = res + s2 - s1;
+                    // Value of current symbol is less than the next symbol
+                    result = result + nextSymbol - currentSymbol;
                     i++;
                 }
             } else {
-                res = res + s1;
+                result = result + currentSymbol;
             }
         }
 
-        return res;
+        return result;
     }
 }
