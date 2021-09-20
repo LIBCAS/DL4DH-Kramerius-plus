02ca00a7e49416f7a16c11f12dc5552e7a282958
Peter Sekan <sekan@inqool.cz>
2021-09-20 08:27:18 +0200

    K8S - More memory for API

--- api/run.sh
+++ api/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /usr/src
-java -jar -Xmx256m api.jar > api.stdout 2> api.stderr
+java -jar -Xmx512m api.jar > api.stdout 2> api.stderr
 
 # Keep docker running, when java was failed.
 tail -f /etc/issue
