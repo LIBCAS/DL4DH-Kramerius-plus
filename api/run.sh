fadb126513e7aa198bc552e1f6bf9ddda043b726
Peter Sekan <sekan@inqool.cz>
2021-10-14 14:14:00 +0200

    K8S - Increased resources

--- api/run.sh
+++ api/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /usr/src
-java -jar -Xmx512m api.jar > api.stdout 2> api.stderr
+java -jar -Xmx1024m api.jar > api.stdout 2> api.stderr
 
 # Keep docker running, when java was failed.
 tail -f /etc/issue
