ec0170a1db2da8eaa49874676843787db47c589e
Noro <bodnar@inqool.cz>
2022-05-06 12:46:04 +0200

    Fix export api path

--- web/src/components/publication/publication-export-dialog.tsx
+++ web/src/components/publication/publication-export-dialog.tsx
@@ -30,7 +30,7 @@ const exportPublication = async (
 	}
 
 	try {
-		const response = await fetch(`/api/exporting/${id}/${format}`, {
+		const response = await fetch(`/api/export/${id}/${format}`, {
 			method: 'POST',
 			headers: new Headers({ 'Content-Type': 'application/json' }),
 			body: JSON.stringify(processedParams),
