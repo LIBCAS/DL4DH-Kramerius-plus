4483782ca9a448fd9c819f3383b8f0bea602cfa6
kollar <filip.kollar@inqool.cz>
2022-07-25 13:35:46 +0200

    added recognition of pdf/epub

--- api/krameriusplus-service/src/main/java/cz/inqool/dl4dh/krameriusplus/service/system/job/config/enrichment/validation/EnrichmentValidationTasklet.java
+++ api/krameriusplus-service/src/main/java/cz/inqool/dl4dh/krameriusplus/service/system/job/config/enrichment/validation/EnrichmentValidationTasklet.java
@@ -1,7 +1,9 @@
 package cz.inqool.dl4dh.krameriusplus.service.system.job.config.enrichment.validation;
 
+import cz.inqool.dl4dh.krameriusplus.core.system.digitalobject.publication.Publication;
 import cz.inqool.dl4dh.krameriusplus.core.system.jobevent.JobEventStore;
 import cz.inqool.dl4dh.krameriusplus.core.system.jobevent.KrameriusJob;
+import cz.inqool.dl4dh.krameriusplus.service.system.dataprovider.kramerius.WebClientDataProvider;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -12,7 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
-import static cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.JobParameterKey.*;
+import static cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.JobParameterKey.JOB_EVENT_ID;
+import static cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.JobParameterKey.KRAMERIUS_JOB;
+import static cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.JobParameterKey.OVERRIDE;
+import static cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.JobParameterKey.PUBLICATION_ID;
 
 @Component
 @StepScope
@@ -31,9 +36,13 @@ public class EnrichmentValidationTasklet implements Tasklet {
 
         String publicationId = jobParameters.getString(PUBLICATION_ID);
         String thisJobEventId = jobParameters.getString(JOB_EVENT_ID);
+        Publication monograph = (Publication) new WebClientDataProvider().getDigitalObject(publicationId);
         KrameriusJob krameriusJob = KrameriusJob.valueOf(jobParameters.getString(KRAMERIUS_JOB));
 
         boolean override = Boolean.parseBoolean(jobParameters.getString(OVERRIDE));
+        if (monograph.getUrl() != null) {
+            throw new IllegalStateException("Publication " + publicationId + " is an EPUB/PDF and cannot be enriched.");
+        }
         if (!override && jobEventStore.existsOtherJobs(publicationId, thisJobEventId, krameriusJob)) {
             throw new IllegalStateException("Job of type '" + krameriusJob + "' for publication '" + publicationId + "' already exists and 'override' is set to false.");
         }
