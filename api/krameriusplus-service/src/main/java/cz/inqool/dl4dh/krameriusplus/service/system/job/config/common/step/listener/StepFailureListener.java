d0af83266dee4fb1e21d742bae5674a63eacd80a
kollar <filip.kollar@inqool.cz>
2022-07-26 16:09:35 +0200

    injected JobEventService, now returns null after job has an error

--- api/krameriusplus-service/src/main/java/cz/inqool/dl4dh/krameriusplus/service/system/job/config/common/step/listener/StepFailureListener.java
+++ api/krameriusplus-service/src/main/java/cz/inqool/dl4dh/krameriusplus/service/system/job/config/common/step/listener/StepFailureListener.java
@@ -1,13 +1,32 @@
 package cz.inqool.dl4dh.krameriusplus.service.system.job.config.common.step.listener;
 
+import cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.JobParameterKey;
+import cz.inqool.dl4dh.krameriusplus.service.system.job.jobevent.JobEventService;
 import lombok.NonNull;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+/**
+ * StepExecutionListener who saves failure into JobExecution for later use
+ *
+ * @author Filip Kollar
+ */
+@StepScope
+@Component
 public class StepFailureListener implements StepExecutionListener {
+    private final JobEventService jobEventService;
+
+    @Autowired
+    public StepFailureListener(JobEventService jobEventService) {
+        this.jobEventService = jobEventService;
+    }
+
     @Override
     public void beforeStep(@NonNull StepExecution stepExecution) {
     }
@@ -18,7 +37,10 @@ public class StepFailureListener implements StepExecutionListener {
         if (failures.isEmpty()) {
             return ExitStatus.COMPLETED;
         }
-        stepExecution.getJobExecution().addFailureException(failures.get(failures.size() - 1));
-        return ExitStatus.FAILED;
+        stepExecution.getJobExecution().addFailureException(failures.get(0));
+        jobEventService.findEntity(stepExecution.getJobParameters().getString(JobParameterKey.JOB_EVENT_ID))
+                .getDetails()
+                .setLastExecutionFailure(failures.get(0).getMessage());
+        return null;
     }
 }
