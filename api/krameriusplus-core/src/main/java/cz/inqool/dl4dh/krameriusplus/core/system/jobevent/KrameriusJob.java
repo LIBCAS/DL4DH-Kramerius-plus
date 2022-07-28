5eacbc48f3aaf5db875ec485c3fb56f271f420a9
Noro <bodnar@inqool.cz>
2022-07-28 17:09:47 +0200

    Added list of job dependencies to KrameriusJob

--- api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/core/system/jobevent/KrameriusJob.java
+++ api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/core/system/jobevent/KrameriusJob.java
@@ -2,25 +2,36 @@ package cz.inqool.dl4dh.krameriusplus.core.system.jobevent;
 
 import lombok.Getter;
 
+import java.util.Collections;
 import java.util.Set;
 
+import static cz.inqool.dl4dh.krameriusplus.core.system.jobevent.JobEventQueue.ENRICHMENT_QUEUE;
+import static cz.inqool.dl4dh.krameriusplus.core.system.jobevent.JobEventQueue.EXPORT_QUEUE;
+
+@Getter
 public enum KrameriusJob {
-    ENRICHMENT_KRAMERIUS(JobEventQueue.ENRICHMENT_QUEUE.getQueueName()),
-    ENRICHMENT_EXTERNAL(JobEventQueue.ENRICHMENT_QUEUE.getQueueName()),
-    ENRICHMENT_NDK(JobEventQueue.ENRICHMENT_QUEUE.getQueueName()),
-    ENRICHMENT_TEI(JobEventQueue.ENRICHMENT_QUEUE.getQueueName()),
+    ENRICHMENT_KRAMERIUS(ENRICHMENT_QUEUE.getQueueName(), Collections.emptySet()),
+    ENRICHMENT_EXTERNAL(ENRICHMENT_QUEUE.getQueueName(), Set.of(ENRICHMENT_KRAMERIUS)),
+    ENRICHMENT_NDK(ENRICHMENT_QUEUE.getQueueName(), Set.of(ENRICHMENT_KRAMERIUS)),
+    ENRICHMENT_TEI(ENRICHMENT_QUEUE.getQueueName(), Set.of(ENRICHMENT_KRAMERIUS)),
 
-    EXPORT_JSON(JobEventQueue.EXPORT_QUEUE.getQueueName()),
-    EXPORT_CSV(JobEventQueue.EXPORT_QUEUE.getQueueName()),
-    EXPORT_TEI(JobEventQueue.EXPORT_QUEUE.getQueueName()),
-    EXPORT_ALTO(JobEventQueue.EXPORT_QUEUE.getQueueName()),
-    EXPORT_TEXT(JobEventQueue.EXPORT_QUEUE.getQueueName());
+    EXPORT_JSON(EXPORT_QUEUE.getQueueName(), Set.of(ENRICHMENT_KRAMERIUS)),
+    EXPORT_CSV(EXPORT_QUEUE.getQueueName(), Set.of(ENRICHMENT_KRAMERIUS)),
+    EXPORT_TEI(EXPORT_QUEUE.getQueueName(), Set.of(ENRICHMENT_KRAMERIUS, ENRICHMENT_TEI)),
+    EXPORT_ALTO(EXPORT_QUEUE.getQueueName(), Collections.emptySet()),
+    EXPORT_TEXT(EXPORT_QUEUE.getQueueName(), Collections.emptySet());
 
-    @Getter
     private final String queueName;
 
-    KrameriusJob(String queueName) {
+    /**
+     * Collection of KrameriusJobs, on which this job is dependent. To start a job of this type,
+     * there must exist at least one job of each type defined in this collection for the same publication.
+     */
+    private final Set<KrameriusJob> dependentOn;
+
+    KrameriusJob(String queueName, Set<KrameriusJob> dependentOn) {
         this.queueName = queueName;
+        this.dependentOn = dependentOn;
     }
 
     public static Set<KrameriusJob> getEnrichingJobs() {
