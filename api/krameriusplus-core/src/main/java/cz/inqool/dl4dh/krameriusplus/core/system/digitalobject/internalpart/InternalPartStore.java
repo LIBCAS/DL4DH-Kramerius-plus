bb2abdf9fd416c4760f69254736d6cfa5390bc56
kollar <filip.kollar@inqool.cz>
2022-08-03 10:06:14 +0200

    removed InternalPartStore

--- api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/core/system/digitalobject/internalpart/InternalPartStore.java
+++
@@ -1,8 +0,0 @@
-package cz.inqool.dl4dh.krameriusplus.core.system.digitalobject.internalpart;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InternalPartStore extends CrudRepository<InternalPart, String> {
-}
