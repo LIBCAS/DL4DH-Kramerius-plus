53d5c3f7c8d0a3c0504bf55c09714f2398cb67a1
kollar <filip.kollar@inqool.cz>
2022-08-25 13:19:34 +0200

    changed all export config dtos

--- api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/core/system/jobeventconfig/dto/export/CsvExportJobConfigDto.java
+++ api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/core/system/jobeventconfig/dto/export/CsvExportJobConfigDto.java
@@ -1,6 +1,5 @@
 package cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.dto.export;
 
-import cz.inqool.dl4dh.krameriusplus.core.domain.dao.mongo.params.Params;
 import cz.inqool.dl4dh.krameriusplus.core.system.jobevent.KrameriusJob;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -14,8 +13,6 @@ public class CsvExportJobConfigDto extends ExportJobConfigDto {
 
     @Schema(description = "Delimiter used to generate export.", defaultValue = ",")
     private String delimiter = ",";
-
-    private Params params = new Params();
 
     @Override
     public KrameriusJob getKrameriusJob() {
