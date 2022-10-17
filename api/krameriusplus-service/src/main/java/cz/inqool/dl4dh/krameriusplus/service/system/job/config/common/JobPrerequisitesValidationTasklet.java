ec9cfd671ffa4e59619e4dce7f222643c8a98d59
kollar <filip.kollar@inqool.cz>
2022-10-17 10:06:39 +0200

    reworked looking for completed jobs on publications

--- api/krameriusplus-service/src/main/java/cz/inqool/dl4dh/krameriusplus/service/system/job/config/common/JobPrerequisitesValidationTasklet.java
+++ api/krameriusplus-service/src/main/java/cz/inqool/dl4dh/krameriusplus/service/system/job/config/common/JobPrerequisitesValidationTasklet.java
@@ -1,8 +1,11 @@
 package cz.inqool.dl4dh.krameriusplus.service.system.job.config.common;
 
 import cz.inqool.dl4dh.krameriusplus.core.domain.exception.ValidationException;
+import cz.inqool.dl4dh.krameriusplus.core.system.digitalobject.DigitalObjectContext;
+import cz.inqool.dl4dh.krameriusplus.core.system.digitalobject.publication.Publication;
 import cz.inqool.dl4dh.krameriusplus.core.system.jobevent.JobEventStore;
 import cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.KrameriusJob;
+import cz.inqool.dl4dh.krameriusplus.service.system.dataprovider.kramerius.DataProvider;
 import cz.inqool.dl4dh.krameriusplus.service.system.job.config.export.common.components.ValidatedTasklet;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.StepContribution;
@@ -13,7 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.JobParameterKey.KRAMERIUS_JOB;
 import static cz.inqool.dl4dh.krameriusplus.core.system.jobeventconfig.JobParameterKey.PUBLICATION_ID;
@@ -24,9 +30,12 @@ public class JobPrerequisitesValidationTasklet extends ValidatedTasklet {
 
     private final JobEventStore jobEventStore;
 
+    private final DataProvider dataProvider;
+
     @Autowired
-    public JobPrerequisitesValidationTasklet(JobEventStore jobEventStore) {
+    public JobPrerequisitesValidationTasklet(JobEventStore jobEventStore, DataProvider dataProvider) {
         this.jobEventStore = jobEventStore;
+        this.dataProvider = dataProvider;
     }
 
     @Override
@@ -34,11 +43,16 @@ public class JobPrerequisitesValidationTasklet extends ValidatedTasklet {
         JobParameters jobParameters = chunkContext.getStepContext().getStepExecution().getJobExecution().getJobParameters();
 
         String publicationId = jobParameters.getString(PUBLICATION_ID);
+        // Workaround for merge job since it is the only job not tied to a publication
+        if (publicationId == null) {
+            return RepeatStatus.FINISHED;
+        }
+        List<String> relevantContext =  getRelevantContext((Publication) dataProvider.getDigitalObject(publicationId));
 
         Set<KrameriusJob> prerequisites = KrameriusJob.valueOf(jobParameters.getString(KRAMERIUS_JOB)).getDependentOn();
 
         for (KrameriusJob prerequisite: prerequisites) {
-            Long count = jobEventStore.getDependency(publicationId, prerequisite);
+            Long count = jobEventStore.getDependency(relevantContext, prerequisite);
             if (count.equals(0L))  {
                 throw new ValidationException(String.format("No completed jobs of type %s for publicationId %s", prerequisite, publicationId),
                         ValidationException.ErrorCode.DEPENDENCY_ERROR);
@@ -46,5 +60,13 @@ public class JobPrerequisitesValidationTasklet extends ValidatedTasklet {
         }
 
         return RepeatStatus.FINISHED;
+    }
+
+    private List<String> getRelevantContext(Publication publication) {
+        Collections.reverse(publication.getContext());
+        return publication.getContext().stream()
+                .map(DigitalObjectContext::getPid)
+                .dropWhile(id -> !id.equals(publication.getId()))
+                .collect(Collectors.toList());
     }
 }
