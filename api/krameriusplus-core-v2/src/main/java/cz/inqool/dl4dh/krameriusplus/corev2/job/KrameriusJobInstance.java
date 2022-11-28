d530e4daccd933163e7b83afe3a3269c2d9f8c94
kollar <filip.kollar@inqool.cz>
2022-11-28 11:33:42 +0100

    reimplemented KrameriusJobInstanceMapper, added javadoc

--- api/krameriusplus-core-v2/src/main/java/cz/inqool/dl4dh/krameriusplus/corev2/job/KrameriusJobInstance.java
+++ api/krameriusplus-core-v2/src/main/java/cz/inqool/dl4dh/krameriusplus/corev2/job/KrameriusJobInstance.java
@@ -8,7 +8,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.batch.core.JobParameters;
 
-import javax.persistence.*;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +29,9 @@ public class KrameriusJobInstance extends DomainObject {
 
     protected Long jobInstanceId;
 
+    /**
+     * StepExecutionId -> StepRunReport
+     */
     @OneToMany
     private Map<Long, StepRunReport> reports = new HashMap<>();
 
