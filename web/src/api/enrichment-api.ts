44de21a5f930a13f7b036e477a6b4d8767f177ee
Noro <bodnar@inqool.cz>
2022-12-31 10:50:49 +0100

    Changed enrichment API call from FE

--- web/src/api/enrichment-api.ts
+++ web/src/api/enrichment-api.ts
@@ -10,7 +10,7 @@ export const enrich = async (
 	name?: string,
 ): Promise<Response> => {
 	const trimmedIds = publicationIds.map(id => id.trim())
-	return await customFetch('/api/enrichment', {
+	return await customFetch('/api/enrichment/enrich', {
 		method: 'POST',
 		body: JSON.stringify({ publicationIds: trimmedIds, name, configs }),
 	})
