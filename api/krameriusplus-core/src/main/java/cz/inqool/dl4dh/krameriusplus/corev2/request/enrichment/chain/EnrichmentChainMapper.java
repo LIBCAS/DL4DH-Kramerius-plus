f603603522286ad5d4350246b0cba3dead4439e2
Noro <bodnar@inqool.cz>
2023-01-03 17:02:00 +0100

    Renamed core-v2 to core

--- api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/corev2/request/enrichment/chain/EnrichmentChainMapper.java
+++ api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/corev2/request/enrichment/chain/EnrichmentChainMapper.java
@@ -3,8 +3,6 @@ package cz.inqool.dl4dh.krameriusplus.corev2.request.enrichment.chain;
 import cz.inqool.dl4dh.krameriusplus.api.enrichment.EnrichmentChainDto;
 import cz.inqool.dl4dh.krameriusplus.corev2.job.KrameriusJobInstanceMapper;
 import org.mapstruct.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @Mapper(uses = {
         KrameriusJobInstanceMapper.class
--- api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/corev2/request/export/export/ExportMapper.java
+++ api/krameriusplus-core/src/main/java/cz/inqool/dl4dh/krameriusplus/corev2/request/export/export/ExportMapper.java
@@ -5,12 +5,6 @@ import cz.inqool.dl4dh.krameriusplus.corev2.file.FileRefMapper;
 import cz.inqool.dl4dh.krameriusplus.corev2.job.KrameriusJobInstanceMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
-
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 @Mapper(uses = {
         FileRefMapper.class,
