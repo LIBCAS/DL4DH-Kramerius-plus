52db5b31c42931685e5fe249b21e9f162ca28a69
Noro <bodnar@inqool.cz>
2023-02-06 15:09:41 +0100

    Implemented bulk publish/unpublish operations

--- api/krameriusplus-rest/src/main/java/cz/inqool/dl4dh/krameriusplus/rest/dto/PublishDto.java
+++ api/krameriusplus-rest/src/main/java/cz/inqool/dl4dh/krameriusplus/rest/dto/PublishDto.java
@@ -1,2 +1,13 @@
-package cz.inqool.dl4dh.krameriusplus.rest.dto;public class PublishDto {
+package cz.inqool.dl4dh.krameriusplus.rest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PublishDto {
+
+    private List<String> publicationIds;
 }
