d563166f05fb84ab508d14d6bdee5161e79e6daf
Noro <bodnar@inqool.cz>
2023-02-14 14:10:11 +0100

    Removed unused job api

--- api/krameriusplus-rest/src/main/java/cz/inqool/dl4dh/krameriusplus/rest/controller/JobApi.java
+++ api/krameriusplus-rest/src/main/java/cz/inqool/dl4dh/krameriusplus/rest/controller/JobApi.java
@@ -1,21 +1,13 @@
 package cz.inqool.dl4dh.krameriusplus.rest.controller;
 
-import cz.inqool.dl4dh.krameriusplus.api.Result;
-import cz.inqool.dl4dh.krameriusplus.api.batch.ExecutionStatus;
-import cz.inqool.dl4dh.krameriusplus.api.batch.KrameriusJobType;
 import cz.inqool.dl4dh.krameriusplus.api.batch.job.KrameriusJobInstanceDto;
-import cz.inqool.dl4dh.krameriusplus.api.job.JobEventFilter;
 import cz.inqool.dl4dh.krameriusplus.api.job.JobFacade;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
-import java.util.Set;
 
 @Tag(name = "Job", description = "Úlohy")
 @RestController
@@ -28,42 +20,6 @@ public class JobApi {
     @Autowired
     public JobApi(JobFacade facade) {
         this.facade = facade;
-    }
-
-    @Operation(summary = "List enrichment jobs.")
-    @GetMapping("/enrichment/list")
-    public Result<KrameriusJobInstanceDto> listEnrichingJobs(@RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
-                                                             @RequestParam(value = "page", defaultValue = "0") int page,
-                                                             @Schema(description = "Optional publicationId parameter. When provided, only enriching jobs for the given publication will be returned.")
-                                                           @RequestParam(value = "publicationId", required = false) String publicationId,
-                                                             @Schema(description = "Optional krameriusJob value. When provided, only enriching jobs of this type will be returned.")
-                                                           @RequestParam(value = "jobType", required = false) KrameriusJobType jobType,
-                                                             @Schema(description = "Optional lastExecutionStatus value. When provided, only enriching jobs with this lastExecutionStatus" +
-                                                               " value will be returned.")
-                                                           @RequestParam(value = "lastExecutionStatus", required = false) ExecutionStatus executionStatus,
-                                                             @Schema(description = "Optional includeDeleted flag. If true, deleted instances will be included. Defaults to false.")
-                                                           @RequestParam(value = "includeDeleted", required = false, defaultValue = "false") boolean includeDeleted) {
-        return facade.listEnrichmentJobs(
-                new JobEventFilter(publicationId,  jobType == null ? Collections.emptySet() : Set.of(jobType),
-                        executionStatus, includeDeleted), page, pageSize);
-    }
-
-    @Operation(summary = "List export jobs.")
-    @GetMapping("/export/list")
-    public Result<KrameriusJobInstanceDto> listExportingJob(@RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
-                                                 @RequestParam(value = "page", defaultValue = "0") int page,
-                                                 @Schema(description = "Optional publicationId parameter. When provided, only exporting jobs for the given publication will be returned.")
-                                                          @RequestParam(value = "publicationId", required = false) String publicationId,
-                                                 @Schema(description = "Optional krameriusJob value. When provided, only exporting jobs of this type will be returned.")
-                                                          @RequestParam(value = "jobType", required = false) KrameriusJobType jobType,
-                                                 @Schema(description = "Optional lastExecutionStatus value. When provided, only exporting jobs with this lastExecutionStatus" +
-                                                              " value will be returned.")
-                                                          @RequestParam(value = "lastExecutionStatus", required = false) ExecutionStatus executionStatus,
-                                                 @Schema(description = "Optional includeDeleted flag. If true, deleted instances will be included. Defaults to false.")
-                                                          @RequestParam(value = "includeDeleted", required = false, defaultValue = "false") boolean includeDeleted) {
-        return facade.listExportJobs(
-                new JobEventFilter(publicationId, jobType == null ? Collections.emptySet() : Set.of(jobType),
-                        executionStatus, includeDeleted), page, pageSize);
     }
 
     @Operation(summary = "Get detail of given Job")
--- web/src/api/job-api.ts
+++ web/src/api/job-api.ts
@@ -5,34 +5,6 @@ import { JobType } from 'enums/job-type'
 import { customFetch } from 'utils/custom-fetch'
 import { KrameriusJobInstance } from 'models/job/kramerius-job-instance'
 
-export const listJobEvents = async (
-	jobType: JobType,
-	page: number,
-	pageSize: number,
-	filter?: JobEventFilterDto,
-): Promise<QueryResults<JobEvent>> => {
-	let url = `/api/jobs/${jobType}/list?page=${page}&pageSize=${pageSize}`
-	url = filter?.publicationId
-		? url + `&publicationId=${filter.publicationId}`
-		: url
-	url = filter?.jobType ? url + `&jobType=${filter.jobType}` : url
-	url = filter?.lastExecutionStatus
-		? url + `&lastExecutionStatus=${filter.lastExecutionStatus}`
-		: url
-	const response = await customFetch(url, {
-		method: 'GET',
-	})
-
-	return await response?.json()
-}
-
-export const getJobEvent = async (jobEventId: string): Promise<JobEvent> => {
-	const response = await customFetch(`/api/jobs/${jobEventId}`, {
-		method: 'GET',
-	})
-	return await response?.json()
-}
-
 export const getJob = async (jobId: string): Promise<KrameriusJobInstance> => {
 	const response = await customFetch(`/api/jobs/${jobId}`, {
 		method: 'GET',
