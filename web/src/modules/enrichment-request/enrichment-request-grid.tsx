c466665580d4e1a8974ad9c835357faa8de4d3d5
Ondrej Kaluzak <kaluzak@inqool.cz>
2024-12-04 22:27:43 +0100

    enrichment requests grid buttons, enrichment profiles

--- web/src/modules/enrichment-request/enrichment-request-grid.tsx
+++ web/src/modules/enrichment-request/enrichment-request-grid.tsx
@@ -1,9 +1,11 @@
+import { FC, useState } from 'react'
 import { Box, Button, Chip, Paper } from '@mui/material'
 import {
 	GridColDef,
 	GridPaginationModel,
 	GridRenderCellParams,
 	GridRowSelectionModel,
+	GridToolbarContainer,
 	GridValueFormatterParams,
 } from '@mui/x-data-grid'
 import { CustomGrid } from 'components/grid/custom-grid'
@@ -15,8 +17,74 @@ import {
 	RequestStateColorMapping,
 	RequestStateMapping,
 } from 'models/request/request'
-import React, { FC, useMemo, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+const columns: GridColDef<EnrichmentRequest>[] = [
+	{
+		field: 'created',
+		headerName: 'Vytvořeno',
+		width: 160,
+		type: 'dateTime',
+		sortable: false,
+		valueGetter: ({ value }) => value && new Date(value),
+	},
+	{
+		field: 'owner',
+		headerName: 'Vytvořil',
+		width: 150,
+		sortable: false,
+		valueFormatter: (params: GridValueFormatterParams<User>) =>
+			params.value.username,
+	},
+	{
+		field: 'name',
+		headerName: 'Název',
+		width: 350,
+		sortable: false,
+	},
+	{
+		field: 'publicationIds',
+		headerName: 'Publikace v žádosti',
+		flex: 1,
+		sortable: false,
+		valueFormatter: (params: GridValueFormatterParams<string[]>) =>
+			params.value.join(', '),
+	},
+	{
+		field: 'state',
+		headerName: 'Stav žádosti',
+		width: 200,
+		sortable: false,
+		renderCell: (params: GridRenderCellParams) => {
+			const state: RequestState = params.row['state']
+			return (
+				<Chip
+					color={RequestStateColorMapping[state]}
+					label={RequestStateMapping[state]}
+					style={{ opacity: '80%' }}
+				/>
+			)
+		},
+	},
+	{
+		field: 'actions',
+		headerName: 'Akce',
+		width: 100,
+		sortable: false,
+		renderCell: (params: GridRenderCellParams) => (
+			<Box display="flex" justifyContent="space-between" width="100%">
+				<Button
+					component={Link}
+					size="small"
+					to={`/enrichment/${params.row['id']}`}
+					variant="text"
+				>
+					Detail
+				</Button>
+			</Box>
+		),
+	},
+]
 
 export const EnrichmentRequestGrid: FC<{
 	isLoading: boolean
@@ -26,84 +94,51 @@ export const EnrichmentRequestGrid: FC<{
 }> = ({ isLoading, data, pagination, onPaginationChange }) => {
 	const [selection, setSelection] = useState<GridRowSelectionModel>([])
 
-	const columns = useMemo<GridColDef<EnrichmentRequest>[]>(
-		() => [
-			{
-				field: 'created',
-				headerName: 'Vytvořeno',
-				width: 160,
-				type: 'dateTime',
-				sortable: false,
-				valueGetter: ({ value }) => value && new Date(value),
-			},
-			{
-				field: 'owner',
-				headerName: 'Vytvořil',
-				width: 150,
-				sortable: false,
-				valueFormatter: (params: GridValueFormatterParams<User>) =>
-					params.value.username,
-			},
-			{
-				field: 'name',
-				headerName: 'Název',
-				width: 350,
-				sortable: false,
-			},
-			{
-				field: 'publicationIds',
-				headerName: 'Publikace v žádosti',
-				flex: 1,
-				sortable: false,
-				valueFormatter: (params: GridValueFormatterParams<string[]>) =>
-					params.value.join(', '),
-			},
-			{
-				field: 'state',
-				headerName: 'Stav žádosti',
-				width: 200,
-				sortable: false,
-				renderCell: (params: GridRenderCellParams) => {
-					const state: RequestState = params.row['state']
-					return (
-						<Chip
-							color={RequestStateColorMapping[state]}
-							label={RequestStateMapping[state]}
-							style={{ opacity: '80%' }}
-						/>
-					)
-				},
-			},
-			{
-				field: 'actions',
-				headerName: 'Akce',
-				width: 100,
-				sortable: false,
-				renderCell: (params: GridRenderCellParams) => (
-					<Box display="flex" justifyContent="space-between" width="100%">
-						<Button
-							component={Link}
-							size="small"
-							to={`/enrichment/${params.row['id']}`}
-							variant="text"
-						>
-							Detail
-						</Button>
-					</Box>
-				),
-			},
-		],
-		[],
-	)
+	const navigate = useNavigate()
 
 	return (
 		<Paper>
 			<CustomGrid
+				checkboxSelection={true}
 				columns={columns}
 				data={data}
 				isLoading={isLoading}
 				pagination={pagination}
 				selection={selection}
+				toolbar={({
+					onClearSelection,
+					onClearCompleted,
+					onEnrchichmentRequestClick,
+				}) => {
+					return (
+						<GridToolbarContainer>
+							<Button onClick={() => onClearSelection?.()}>Zrušit výběr</Button>
+							<Button onClick={() => onClearCompleted?.()}>
+								Zrušit výběr dokončených
+							</Button>
+							<Button onClick={() => onEnrchichmentRequestClick?.()}>
+								Obohatit označené
+							</Button>
+						</GridToolbarContainer>
+					)
+				}}
+				toolbarProps={{
+					onClearSelection: () => setSelection([]),
+					onClearCompleted: () =>
+						setSelection(prev => {
+							const filtered = prev.filter(
+								id => data?.items.find(p => p.id === id)?.state !== 'COMPLETED',
+							)
+							return filtered
+						}),
+					onEnrchichmentRequestClick: () => {
+						const requests = selection
+							.map(id => data?.items.find(item => id === item.id))
+							.filter(f => f !== undefined) as EnrichmentRequest[]
+
+						navigate('/enrichment/new', { state: { requests } })
+					},
+				}}
 				onPaginationChange={onPaginationChange}
 				onSelectionChange={setSelection}
 			/>
--- web/src/pages/enrichment/enrichment-page.tsx
+++ web/src/pages/enrichment/enrichment-page.tsx
@@ -1,11 +1,24 @@
-import { Button, Divider, Grid, Paper, TextField } from '@mui/material'
+import {
+	Button,
+	Divider,
+	Grid,
+	Paper,
+	Tab,
+	Tabs,
+	TextField,
+	Typography,
+} from '@mui/material'
 import { enrich } from 'api/enrichment-api'
 import { PageBlock } from 'components/page-block'
 import { EnrichmentJobConfig } from 'models/job/config/enrichment-job-config'
 import { EnrichmentConfigs } from 'modules/enrichment-form/enrichment-configs'
-import { ChangeEvent, FC, useState } from 'react'
+import { ChangeEvent, FC, useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 import { PageWrapper } from '../page-wrapper'
+import { EnrichmentKrameriusJob } from 'enums/enrichment-kramerius-job'
+import { v4 as uuid } from 'uuid'
+
+import { useLocation } from 'react-router-dom'
 
 type StateType = {
 	publicationIds: string
@@ -19,8 +32,95 @@ const initialState = {
 	name: '',
 }
 
+export enum EnrichmentProfile {
+	BASIC = 'BASIC',
+	UPDATE = 'UPDATE',
+	COMPLETE = 'COMPLETE',
+	CUSTOM = 'CUSTOM',
+}
+
+type EnrichmentProfileType = {
+	type: EnrichmentProfile
+	configs: EnrichmentJobConfig[]
+	label: string
+}
+
+const profiles: EnrichmentProfileType[] = [
+	{
+		type: EnrichmentProfile.BASIC,
+		configs: [
+			{
+				id: uuid(),
+				override: false,
+				pageErrorTolerance: 0,
+				jobType: EnrichmentKrameriusJob.ENRICHMENT_EXTERNAL,
+			},
+			{
+				id: uuid(),
+				override: false,
+				pageErrorTolerance: 0,
+				jobType: EnrichmentKrameriusJob.ENRICHMENT_TEI,
+			},
+		],
+		label: 'Základ',
+	},
+	{
+		type: EnrichmentProfile.UPDATE,
+		configs: [
+			{
+				id: uuid(),
+				override: true,
+				pageErrorTolerance: 0,
+				jobType: EnrichmentKrameriusJob.ENRICHMENT_EXTERNAL,
+			},
+			{
+				id: uuid(),
+				override: true,
+				pageErrorTolerance: 0,
+				jobType: EnrichmentKrameriusJob.ENRICHMENT_TEI,
+			},
+		],
+		label: 'Aktualizace',
+	},
+	{
+		type: EnrichmentProfile.COMPLETE,
+		configs: [
+			{
+				id: uuid(),
+				override: true,
+				pageErrorTolerance: 0,
+				jobType: EnrichmentKrameriusJob.ENRICHMENT_EXTERNAL,
+			},
+			{
+				id: uuid(),
+				override: true,
+				pageErrorTolerance: 0,
+				jobType: EnrichmentKrameriusJob.ENRICHMENT_TEI,
+			},
+			{
+				id: uuid(),
+				override: true,
+				pageErrorTolerance: 0,
+				jobType: EnrichmentKrameriusJob.ENRICHMENT_NDK,
+			},
+		],
+		label: 'Komplet',
+	},
+	{
+		type: EnrichmentProfile.CUSTOM,
+		configs: [],
+		label: 'Vlastní',
+	},
+]
+
 export const EnrichmentPage: FC = () => {
-	const [state, setState] = useState<StateType>({ ...initialState })
+	const location = useLocation()
+	const [state, setState] = useState<StateType>({
+		...initialState,
+		name: (location?.state?.requests ?? [])?.[0]?.name,
+		publicationIds: (location?.state?.requests ??
+			[])?.[0]?.publicationIds?.join?.(', '),
+	})
 
 	const setConfigs = (configs: EnrichmentJobConfig[]) => {
 		setState(prev => ({ ...prev, configs: configs }))
@@ -62,6 +162,15 @@ export const EnrichmentPage: FC = () => {
 	const onNameChange = (event: ChangeEvent<HTMLInputElement>) => {
 		setState(prev => ({ ...prev, name: event.target.value }))
 	}
+	const [selectedProfile, setProfile] = useState<EnrichmentProfileType>(
+		profiles[0],
+	)
+
+	useEffect(() => {
+		if (selectedProfile.type !== EnrichmentProfile.CUSTOM) {
+			setConfigs(selectedProfile.configs)
+		}
+	}, [selectedProfile])
 
 	return (
 		<PageWrapper requireAuth>
@@ -99,10 +208,27 @@ export const EnrichmentPage: FC = () => {
 									<Divider />
 								</Grid>
 								<Grid item xs={12}>
-									<EnrichmentConfigs
-										configs={state.configs || []}
-										setConfigs={setConfigs}
-									/>
+									<Typography variant="h6">Profil obohacení</Typography>
+									<Tabs
+										value={selectedProfile.type}
+										onChange={props => console.log({ props })}
+									>
+										{profiles.map((profile, index) => (
+											<Tab
+												key={index}
+												label={profile.label}
+												style={{ width: '25%' }}
+												value={profile.type}
+												onClick={() => setProfile(profile)}
+											/>
+										))}
+									</Tabs>
+									{selectedProfile.type === EnrichmentProfile.CUSTOM && (
+										<EnrichmentConfigs
+											configs={state.configs || []}
+											setConfigs={setConfigs}
+										/>
+									)}
 								</Grid>
 								<Grid item xs={12}>
 									<Divider />
