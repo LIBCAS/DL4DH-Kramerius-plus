9e336f2f39ec41fec5d52ed9c7d9d7402943ddaf
Peter Sekan <sekan@inqool.cz>
2025-04-30 10:39:27 +0200

    Do not allow empty publication ids.

--- api/krameriusplus-api/src/main/java/cz/inqool/dl4dh/krameriusplus/api/RequestCreateDto.java
+++ api/krameriusplus-api/src/main/java/cz/inqool/dl4dh/krameriusplus/api/RequestCreateDto.java
@@ -4,6 +4,7 @@ import cz.inqool.dl4dh.krameriusplus.api.domain.DatedObjectCreateDto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,5 +20,6 @@ public abstract class RequestCreateDto extends DatedObjectCreateDto {
     /**
      * List of root publication UUIDs to process in the request
      */
+    @NotEmpty(message = "PublicationIds cannot be empty.")
     private List<String> publicationIds = new ArrayList<>();
 }
